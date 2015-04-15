acaf6758057c6449e0cbcb3a742ad0010afe27ce
Ludger Peters <ludjer@gmail.com>
2015-04-15 09:17:50 +0200

    added the apache license header to all my files.

--- src/test/java/com/ludgerpeters/AppTest.java
+++ src/test/java/com/ludgerpeters/AppTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 Ludger Peters
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.ludgerpeters;
 
 import junit.framework.Test;
@@ -12,7 +28,7 @@ public class AppTest
 {
     /**
      * Create the test case
-     * asd123asd123
+     *
      * @param testName name of the test case
      */
     public AppTest( String testName )
